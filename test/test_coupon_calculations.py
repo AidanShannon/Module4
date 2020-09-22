e732dd072c34975c4507edf68e36b87b5ac131fe
Aidan Shannon <ajshannon@dmacc.edu>
2020-09-21 22:50:11 -0500

    2nd set of tests pass

--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -12,12 +12,13 @@ class MyTestCase(unittest.TestCase):
         self.assertAlmostEqual(2.57, coupon.calculate_price(6.01, 10, 20), 2)
 
     def test_price_under_between_ten_thirty(self):
-        self.assertAlmostEqual(0, coupon.calculate_price(10, 5, 10), 2)
-        self.assertAlmostEqual(0, coupon.calculate_price(20, 5, 15), 2)
-        self.assertAlmostEqual(0, coupon.calculate_price(30, 5, 20), 2)
-        self.assertAlmostEqual(0, coupon.calculate_price(10, 10, 10), 2)
-        self.assertAlmostEqual(0, coupon.calculate_price(20, 10, 15), 2)
-        self.assertAlmostEqual(0, coupon.calculate_price(30, 10, 20), 2)
+        self.assertAlmostEqual(10.72, coupon.calculate_price(10, 5, 10), 2)
+        self.assertAlmostEqual(21.47, coupon.calculate_price(20, 5, 15), 2)
+        self.assertAlmostEqual(29.15, coupon.calculate_price(30, 5, 20), 2)
+        self.assertAlmostEqual(5.95, coupon.calculate_price(10, 10, 10), 2)
+        self.assertAlmostEqual(14.96, coupon.calculate_price(20, 10, 15), 2)
+        self.assertAlmostEqual(24.91, coupon.calculate_price(30, 10, 20), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
