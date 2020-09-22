e30a4eab29f3c583ce73e880983e31f6faa2b156
Aidan Shannon <ajshannon@dmacc.edu>
2020-09-21 22:44:27 -0500

    2nd set of tests fail

--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -11,7 +11,13 @@ class MyTestCase(unittest.TestCase):
         self.assertAlmostEqual(1.45, coupon.calculate_price(5, 10, 15), 2)
         self.assertAlmostEqual(2.57, coupon.calculate_price(6.01, 10, 20), 2)
 
-
+    def test_price_under_between_ten_thirty(self):
+        self.assertAlmostEqual(0, coupon.calculate_price(10, 5, 10), 2)
+        self.assertAlmostEqual(0, coupon.calculate_price(20, 5, 15), 2)
+        self.assertAlmostEqual(0, coupon.calculate_price(30, 5, 20), 2)
+        self.assertAlmostEqual(0, coupon.calculate_price(10, 10, 10), 2)
+        self.assertAlmostEqual(0, coupon.calculate_price(20, 10, 15), 2)
+        self.assertAlmostEqual(0, coupon.calculate_price(30, 10, 20), 2)
 
 if __name__ == '__main__':
     unittest.main()
