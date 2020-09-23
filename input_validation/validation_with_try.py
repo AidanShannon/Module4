3e9e62650afaed7408736d3a2ee34f855b01b04e
Aidan Shannon <ajshannon@dmacc.edu>
2020-09-22 20:58:15 -0500

    initial commit, test fail

---
+++ input_validation/validation_with_try.py
@@ -0,0 +1,11 @@
+"""
+
+
+"""
+NUMBER_TESTS = 3
+
+
+def average(score1, score2, score3):
+    if score1 < 0 or score2 < 0 or score3 < 0:
+        return float((score1 + score2 + score3)/NUMBER_TESTS)
+
